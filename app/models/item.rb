0621a450ae6384669c75945e7ed1917aef606355
umbsa <sdmktkg@gmail.com>
2021-04-27 17:26:16 +0900

    バリデーションの定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  validates :genre_id, numericality: { other_than: 1 } 
+
+  has_one_attached :image
+
+  belongs_to :user
+  has_one    :order
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :info
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/,
+      message: "半角数字のみ使えます" }
+  end
+
 end
--- db/migrate/20210427063130_create_items.rb
+++ db/migrate/20210427063130_create_items.rb
@@ -7,8 +7,10 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :status_id,       null: false
       t.integer :shipping_fee_id, null: false
       t.integer :prefecture_id,   null: false
+      t.integer :scheduled_delivery_id, null: false
       t.integer :price,           null: false
       t.references :user, foreign_key: true
+      t.integer :genre_id, null: false
       t.timestamps
     end
   end
