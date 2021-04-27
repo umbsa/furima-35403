54c4c3864cfd5f2e65d76b6d039f02eafeb7ea1e
umbsa <sdmktkg@gmail.com>
2021-04-27 15:41:02 +0900

    itemモデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
   end
 
-  # def new
-  # end
+  def new
+  end
 
-  # def create
-  # end
+  def create
+  end
 end
