bf288d3aa793310fdd5edbdba3769fb382167522
umbsa <sdmktkg@gmail.com>
2021-04-30 19:36:29 +0900

    商品購入機能の修正

--- spec/factories/order_inputs.rb
+++ spec/factories/order_inputs.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     city          {'練馬区月見台'}
     addresses     {'すすきヶ原2-23'}
     phone_number  {"09012345678"}
+    building      {'月見台ビル'}
     token {"tok_abcdefghijk00000000000000000"}
   end
 end
