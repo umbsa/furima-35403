c7ddc4ada5454dd846883287bad66b32356ba904
umbsa <sdmktkg@gmail.com>
2021-04-30 18:13:00 +0900

    Formオブジェクトパターンの導入

---
+++ app/models/input.rb
@@ -0,0 +1,3 @@
+class Input < ApplicationRecord
+  belongs_to :order
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
-  #has_one    :order
+  has_one    :order
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
